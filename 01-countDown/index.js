6845f77cea3e3d456f472726b405223b1f95cf4c
Sumina <suminaawa123@gmail.com>
2022-10-05 09:55:31 +0545

    01 CountDown

--- 01-countDown/index.js
+++ 01-countDown/index.js
@@ -1,1 +1,26 @@
-let= "get done here"
+
+var countDownElement = document.getElementById('countdown');
+var bgImageElement = document.getElementById('bg-image');
+
+var initalCountdownValue = countDownElement.innerHTML;
+
+setInterval(function() {
+    initalCountdownValue = initalCountdownValue > 0? initalCountdownValue -1 : 0;
+
+    countDownElement.innerHTML = initalCountdownValue;
+    var backImgPath = initalCountdownValue % 2 == 0?  "image/image1.png" :  'image/image2.png'
+//console.log(1) :console.log(2)
+    bgImageElement.src = backImgPath;
+   
+},1000);   //1000ms so per second
+
+
+
+
+
+
+
+
+
+
+
